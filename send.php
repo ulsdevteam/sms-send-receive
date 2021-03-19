3a7fc2d31654a698966d1af8d20c94c9ab3bc690
wopsononock <rlh52@pitt.edu>
2021-03-19 10:18:37 -0400

    added documentation changed error condition and attempted fix log location

--- send.php
+++ send.php
@@ -9,36 +9,46 @@ if (isset($_SERVER['SERVER_NAME'])) {
 \Telnyx\Telnyx::setApiKey(TELNYX_API_KEY);
 
 $your_telnyx_number = TELNYX_PHONE_NUM;
-//loop and send all the new letters here
+//letters from alma stored here:
 $csvFile = file('/var/local/patron/sms/pending/messages.csv');
 $data = [];
 $errorCount=0;
 
+/**
+ * Calls the Telnyx Message Create method and interprets the response
+ * @param string $your_telnyx_number Telnyx number that will send the messages.
+ * @param string $destination_number Recipient's phone number from messages.csv.
+ * @param string $message Content of the outgoing SMS from messages.csv.
+ * @throws Exception if the response object includes errors
+ * @return bool
+ */
 function askTelnyxToSendSMS($your_telnyx_number,$destination_number,$message){
 	$response = \Telnyx\Message::Create(['from' => $your_telnyx_number, 'to' => $destination_number, 'text' => $message]);
 	$lastResponse = $response->getLastResponse();
-	if ($lastResponse && $lastResponse->code === 200 && isset($lastResponse->json) && isset($lastResponse->json['status']) && $lastResponse->json['status']=='delivered') {
+	if ($lastResponse && $lastResponse->code === 200 && isset($lastResponse->json) && $lastResponse->json['errors'] !== false) {
 		//echo $decoded->{'data'}->{'record_type'};
 		//var_dump($response);
 		//echo $lastResponse->json['received_at'];
-		var_dump($response);
+		echo 'Success!'.PHP_EOL;
+		print_r($response);
 		return true;
 	} else {
-		var_dump($response);
-		throw new Exception("Failed to send. HTTP: $lastResponse->code. $destination_number. $message");
+		print_r($response);
+		throw new Exception('"'.$destination_number.'"'.'"'. $message.'"');
 	}
 }
 //attempt all the sends and log any errors
 foreach ($csvFile as $line) {
+	//put the csv line into an array
 	$data[] = str_getcsv($line);
+	//array will have two elements per index, the phone number and the message
 	$destination_number = '+'.$data[0][0];
 	$message = $data[0][1];
 	//echo $destination_number.' '.$message;
 	try {
 		askTelnyxToSendSMS($your_telnyx_number,$destination_number,$message);
-		$success++;
 	} catch (Exception $e) {
-		$failureLog = fopen("error/failedToSend.csv", 'a+');
+		$failureLog = fopen("/var/local/patron/sms/error/failedToSend.csv", 'a+');
 		fwrite($failureLog,$e->getMessage()); 
 		$errorCount++;
 	}
